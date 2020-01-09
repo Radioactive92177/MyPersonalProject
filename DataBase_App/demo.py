4b65ba118b0fc2d1325601a807b600db4ca2a2de
Raj <56403033+Radioactive92177@users.noreply.github.com>
2020-01-10 01:46:04 +0530

    final Model

--- DataBase_App/demo.py
+++ DataBase_App/demo.py
@@ -1,4 +1,22 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname="postgres", user="postgres", password="6502", host="localhost")
-print("Connection Success")
+
+def create():
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password="6502", host="localhost")
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE DEMO_Table(Name text, Age text, Address text);''')
+    print("Table Created")
+    conn.commit()
+    conn.close()
+
+
+def insert_data():
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password="6502", host="localhost")
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO demo_table(name, age, address) VALUES ('John',25,'NY');''')
+    print("Data Inserted")
+    conn.commit()
+    conn.close()
+
+
+insert_data()
